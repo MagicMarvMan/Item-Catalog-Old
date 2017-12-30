a051be7d30ca4e503e437899043d148e90997fcf
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2017-12-30 14:02:38 +0100

    Added login page with Google SignIn

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
 	return render_template("home.html",title="Home")
 
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
 if(__name__ == '__main__'):
 	app.run(host="0.0.0.0")
