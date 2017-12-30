dfd4c98e9021f10abc32e9b47c75707b57449aa5
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2017-12-30 17:38:38 +0100

    Improved error_handlers

--- app.py
+++ app.py
@@ -29,20 +29,20 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.errorhandler(404)
-def 404():
-	return render_template("error.html",error=404)
+def 404(e):
+	return(render_template("error.html",error=404),404)
 
 @app.errorhandler(410)
-def 410():
-	return render_template("error.html",error=410)
+def 410(e):
+	return(render_template("error.html",error=410),410)
 
 @app.errorhandler(403)
-def 403():
-	return render_template("error.html",error=403)
+def 403(e):
+	return(render_template("error.html",error=403),403)
 
 @app.errorhandler(500)
-def 500():
-	return render_template("error.html",error=500)
+def 500(e):
+	return(render_template("error.html",error=500),500)
 
 
 @app.route("/")
