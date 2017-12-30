7d0696e1f4e9bad7935297a2f73750a70b84f0f3
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2017-12-30 13:31:11 +0100

    Fixed template naming error

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html",title="Home")
+	return render_template("home.html",title="Home")
 
 if(__name__ == '__main__'):
 	app.run(host="0.0.0.0")
