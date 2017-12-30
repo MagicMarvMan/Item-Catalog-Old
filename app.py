41288809f455594fead4b5f1ad29f872a33cea71
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2017-12-30 13:28:19 +0100

    Added templates

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def index():
-	return "Hello World!"
+	return render_template("index.html",title="Home")
 
 if(__name__ == '__main__'):
 	app.run(host="0.0.0.0")
