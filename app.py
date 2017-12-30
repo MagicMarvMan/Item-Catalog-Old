0b268d617d40f370b033511048cff795c43c5e48
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2017-12-30 18:43:55 +0100

    Added utf-8 to bytes

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def fbconnect():
         and replace the remaining quotes with nothing so that it can be used directly in the graph
         api calls
     '''
-    token = bytes(bytes(result).split(',')[0].split(':')[1].replace('"', ''))
+    token = bytes(bytes(result,'utf-8').split(',')[0].split(':')[1].replace('"', ''),'utf-8')
 
     url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
     h = httplib2.Http()
